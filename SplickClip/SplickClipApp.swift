44fb9b90a784093e83d08ddc68001a57a03d4b30
tq.hoan03 <thaihoang03082003@gmail.com>
2026-09-10 20:14:35 +0700

    feat(ios): add app clip target and initial configuration

---
+++ SplickClip/SplickClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplickClipApp.swift
+//  SplickClip
+//
+//  Created by Trahn Quoc Hoan on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SplickClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
